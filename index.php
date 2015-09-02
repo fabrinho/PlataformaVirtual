318badaf5870f077f1e8d8de87d2159719d609b4
fabrinho <faranpe@gmail.com>
2015-09-02 12:37:40 -0400

    Index subido a github

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Plataforma Virtual</title>
+    </head>
+    <body>
+        <h1>Plataforma Virtual de Aprendizaje</h1>
+        <p>Bienvenido, mil disculpas aun estamos trabajando, Gracias.</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
